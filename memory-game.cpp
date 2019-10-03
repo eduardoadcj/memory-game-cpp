571dfd922c6dedb1cbac94abb0ac46d9a1134663
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2019-10-03 01:23:00 -0300

    implementacoes iniciais

---
+++ memory-game.cpp
@@ -0,0 +1,22 @@
+#ifdef __MINGW32__
+	#define SDL_MAIN_HANDLED
+#endif
+
+#include <SDL.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <random>
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+
+#define SCREEN_W 1360
+#define SCREEN_H 768
+
+int main()
+{
+    
+}
---
+++ renderer.h
@@ -0,0 +1,14 @@
+
+struct point
+{
+    double x, y;
+};
+
+class obj
+{
+    public:
+        point position;
+        double w, h;      
+};
+
+void render();
